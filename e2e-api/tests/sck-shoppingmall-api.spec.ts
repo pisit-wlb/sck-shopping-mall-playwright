9149fdd269799983303deebfbefc75ddc04de925
pisit-wlb <pisit@welovebug.com>
2026-02-23 16:50:30 +0700

    Update UI Test Source Code

--- e2e-api/tests/sck-shoppingmall-api.spec.ts
+++ e2e-api/tests/sck-shoppingmall-api.spec.ts
@@ -1,8 +1,7 @@
 import { test, expect, request } from '@playwright/test';
 
 test("ค้นหาและซื้อสินค้า เลือกวิธีจัดส่งเป็น Thai Post และชำระเงินด้วยบัตรเครดิต Visa สำเร็จ", async ({ request }) => {
-
-	let 
+	
 	await test.step("ดำเนินการเข้าสู่ระบบด้วย Username และ Password", async () => {
 		const reponseLogin = await request.post('http://139.59.225.96/api/v1/login', {
 			data : {
