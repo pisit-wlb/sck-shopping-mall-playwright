80f7f0e990fa0ce26db4d8b9418b1e5c6ca4ab04
pisit-wlb <pisit@welovebug.com>
2026-02-24 13:45:08 +0700

    [Added]: สเพิ่ม Test Step การ Login ผ่านทาง API ใน e2e-api/tests/sck-shopping-mall-api.spec.ts

--- e2e-api/tests/sck-shopping-mall-api.spec.ts
+++ e2e-api/tests/sck-shopping-mall-api.spec.ts
@@ -2,4 +2,15 @@ import { test, expect, request } from '@playwright/test';
 
 test("การสั่งซื้อสินค้า โดยที่มีการจัดส่งแบบ Thai Post และชำระเงินด้วยบัตรเครดิต Visa สำเร็จ", async ( { request } ) => {
 
+	test.step("ดำเนินการเข้าสู่ระบบด้วย Username และ Password", async () => {
+		const responseLogin = await request.post("http://139.59.225.96/api/v1/login", {
+			data: {
+				"username" : "user_4",
+				"password" : "P@ssw0rd"
+			}
+		});
+		expect(responseLogin.ok()).toBeTruthy();
+		expect((await responseLogin.json()).access_token).toBeTruthy();
+	});
+
 });
