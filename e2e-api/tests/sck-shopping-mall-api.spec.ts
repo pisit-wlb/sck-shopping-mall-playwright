6215a75997de3ee54d12fed17759c0be2f227bc2
pisit-wlb <pisit@welovebug.com>
2026-02-24 13:06:39 +0700

    [Created]: สร้างไฟล์ใหม่สำหรับการทดสอบ E2E Flow ด้วย API ใน e2e-api/tests/sck-shopping-mall-api.spec.ts

---
+++ e2e-api/tests/sck-shopping-mall-api.spec.ts
@@ -0,0 +1,5 @@
+import { test, expect, request } from '@playwright/test';
+
+test("การสั่งซื้อสินค้า โดยที่มีการจัดส่งแบบ Thai Post และชำระเงินด้วยบัตรเครดิต Visa สำเร็จ", async ( { request } ) => {
+
+});
