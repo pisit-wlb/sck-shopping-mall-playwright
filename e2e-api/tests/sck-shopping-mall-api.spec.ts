57e7348c3d29690cabfca3fe6ce02551ab5b66d1
pisit-wlb <pisit@welovebug.com>
2026-02-24 14:12:15 +0700

    [Added]: เพิ่ม Test st การต้นหาสินค้า ใน e2e-api/tests/sck-shopping-mall-api.spec.ts

--- e2e-api/tests/sck-shopping-mall-api.spec.ts
+++ e2e-api/tests/sck-shopping-mall-api.spec.ts
@@ -2,15 +2,24 @@ import { test, expect, request } from '@playwright/test';
 
 test("การสั่งซื้อสินค้า โดยที่มีการจัดส่งแบบ Thai Post และชำระเงินด้วยบัตรเครดิต Visa สำเร็จ", async ( { request } ) => {
 
-	test.step("ดำเนินการเข้าสู่ระบบด้วย Username และ Password", async () => {
-		const responseLogin = await request.post("http://139.59.225.96/api/v1/login", {
-			data: {
-				"username" : "user_4",
-				"password" : "P@ssw0rd"
-			}
-		});
-		expect(responseLogin.ok()).toBeTruthy();
-		expect((await responseLogin.json()).access_token).toBeTruthy();
+	const responseLogin = await request.post("http://139.59.225.96/api/v1/login", {
+		data: {
+			"username" : "user_4",
+			"password" : "P@ssw0rd"
+		}
 	});
+	expect(responseLogin.ok()).toBeTruthy();
+	expect((await responseLogin.json()).access_token).toBeTruthy();
+	const accessToken = (await responseLogin.json()).access_token;
+	
+	const responseSearchProduct = await request.get("http://139.59.225.96/api/v1/product?q=Bicycle&offset=0&limit=20", {
+		headers: {
+		"Authorization" : "Bearer " + accessToken,
+		}
+	}
+	);
+	expect(responseSearchProduct.ok()).toBeTruthy();
+	expect((await responseSearchProduct.json()).products[0].product_name).toBe("Balance Training Bicycle");
+	expect((await responseSearchProduct.json()).products[0].product_price_thb).toBe(4314.6);
 
 });
